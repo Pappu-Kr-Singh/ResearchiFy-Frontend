0144d4bee429da368c12ce281c348203a68c7b92
Pappu Singh <pappu.singh159687@gmail.com>
2024-08-01 11:40:18 +0530

    added some changes

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -56,7 +56,7 @@ function SideBar({ isAuthenticated }) {
                 <svg className="bi pe-none me-2" width="16" height="16">
                   <use xlinkHref="#people-circle"></use>
                 </svg>
-                Profile
+                Your Todo
               </Link>
             </li>
           )}
